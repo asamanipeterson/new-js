fac4440cd94a7c0b7713b47030245261e2b24901
denzelboye <denzelthmone21@gmail.com>
2025-04-28 12:13:17 +0000

    Js Day2

---
+++ Day2.js
@@ -0,0 +1,44 @@
+//controll Flow
+//if else condition
+// let age =18;
+// if(age >= 18){
+//     console.log(`You are an adult`)
+// }else{
+//     console.log(`You are a minor`)
+// }
+//if else if condition
+// let   score =85;
+
+// if(score >= 90){
+//     console.log(`Grade:A`)
+// }else if(score>=80){
+//     console.log(`Grade:B`)
+// }else if(score>=70){
+//     console.log(`Grade:C`)
+// }else{
+//     console.log(`Grade:D`)
+// }
+
+// let result = age >=18 ?`Adult`:`Minor`
+
+
+// for( let i = 0;i < 11; i ++){
+//     console.log(i);
+// }
+let mixed =[1,3,5,`Hello`,true,2.5]
+//I can use this instead of for(let i=0;i<mixed.length;i++)and the mixed.length is used to count the number of items in the array
+// for(num of mixed){
+//     console.log(typeof(num))
+//     console.log(num)
+// }
+
+//Using a while loop
+let i=0;
+// while(i<mixed.length){
+//     console.log(mixed[i])
+//     i++
+// }
+//Using a do while
+do{
+console.log(mixed[i])
+}while(i<mixed.length);
---
+++ Day2exe.js
@@ -0,0 +1,37 @@
+let score =88;
+let grade;
+if(score>=90){
+    grade=`A`
+    console.log(`Grade:${grade}`)
+}else if(score>=80){
+    grade=`B`
+    console.log(`Score:${score} Grade:${grade}`)
+}else if(score>=70){
+    grade=`C`
+    console.log(`Grade:${grade}`)
+}
+else{
+    grade=`F`
+    console.log(`Grade:${grade}`)
+}
+
+//Exercise 2
+const  numbers = [10, 15, 20, 25, 30];
+let i=0;
+for(;i<numbers.length;i++){
+    if(numbers[i]%5===0){
+        console.log(numbers[i])
+    };
+}
+console.log(`let's intorduce a braek here`)
+for(num of numbers){
+    if(num%5===0){
+        console.log(num)
+    }
+}
+console.log(`let's intorduce a braek here`)
+numbers.forEach(item=>{
+    if(item%5===0){
+        console.log(item)
+    }
+})
